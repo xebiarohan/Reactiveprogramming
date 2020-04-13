260e203a0a8877b9bab42938433a71b0d08b78cc
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2020-04-13 20:55:23 +0400

    Observable examples

--- src/main/java/com/reactiveprogramming/reactiveprogramming/observsables/ObservablesExamples.java
+++ src/main/java/com/reactiveprogramming/reactiveprogramming/observsables/ObservablesExamples.java
@@ -1,6 +1,8 @@
 package com.reactiveprogramming.reactiveprogramming.observsables;
 
 import io.reactivex.Observable;
+import io.reactivex.observables.ConnectableObservable;
+import io.reactivex.subjects.PublishSubject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/observables")
 public class ObservablesExamples {
 
     @GetMapping("/examples")
-    public ResponseEntity<Void> getFirstObservable() {
+    public ResponseEntity<Void> getFirstObservable() throws InterruptedException {
         String[] letters = {"a", "b", "c", "d", "e", "f", "g"};
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 45);
 
@@ -67,10 +70,53 @@ public class ObservablesExamples {
         //  Empty Observable
                 Observable.empty();
 
-        //  ConditionalOperator
+        //  ConditionalOperator   defaultIfEmpty
         Observable.empty()
                 .defaultIfEmpty(2)
                 .subscribe(x -> System.out.println(x));
+
+
+        // getting first Value conditionally from observable
+        Observable.fromArray(letters)
+                .first("default value")
+                .subscribe(x -> System.out.println(x));
+
+
+        //  If value comes then print otherwise nothing will print
+        Observable.empty()
+                .firstElement()
+                .subscribe(x -> System.out.println(x));
+
+        // takeWhile, Contain, SkipWhile, SkipUntil, TakeUntil
+
+        Observable.fromIterable(list)
+                .takeWhile(i -> i> 2)
+                .subscribe(x -> System.out.println(x));
+
+
+
+        // Connectable Operator
+        //A ConnectableObservable resembles an ordinary Observable, except that it doesn't begin
+        // emitting items when it is subscribed to, but only when the connect operator is applied to it
+
+        ConnectableObservable<Long> connectable
+                = Observable.interval(200, TimeUnit.MILLISECONDS)
+                .take(5)
+                .publish();
+        connectable.subscribe(i -> System.out.println(i));
+
+        connectable.connect();
+        Thread.sleep(500);
+
+        // Subject  acts as a Observable and subscriber
+        PublishSubject<Integer> subject = PublishSubject.create();
+        subject.onNext(1);
+        subject.onNext(2);
+        subject.subscribe(x -> System.out.println("value of first Observable " + x));
+        subject.onNext(10);
+        subject.subscribe(y -> System.out.println("value of second Observable " + y));
+
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
