efead9759968127f0d809a0ff7bcf2d34ffdafca
Rohan Aggarwal <aggarwal.rohan17@gmail.com>
2020-05-28 21:47:51 +0400

    Flux examples

--- src/main/java/com/reactiveprogramming/reactiveprogramming/observsables/FluxExamples.java
+++ src/main/java/com/reactiveprogramming/reactiveprogramming/observsables/FluxExamples.java
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/observables")
@@ -20,15 +23,34 @@ public class FluxExamples {
 
     @GetMapping("/flux")
     public ResponseEntity<Void> fluxExample() {
-        List<String> iterable = Arrays.asList("foo", "bar", "foobar");
+        List<String> list = Arrays.asList("foo", "bar", "foobar");
 
         // Flux.just
-        Flux<Integer> primaryFlux = Flux.just(1, 2, 3, 4);
+        Flux<Object> primaryFlux = Flux.just(1, 2, 3, 4, new Exception("Something went wrong"));
         primaryFlux.subscribe(val -> System.out.println(val));
 
+        Mono<Integer> just = Mono.just(1);
+
+        Mono<Object> empty = Mono.empty();
+
+        Mono<Object> exception = Mono.error(new Exception("Something went wrong"));
+
+        Mono<Object> from = Mono.from(primaryFlux);
+
+        Mono<Object> justOrEmptyMono1 = Mono.justOrEmpty(Optional.ofNullable(null));
+        Mono<Integer> justOrEmptyMono2 = Mono.justOrEmpty(Optional.ofNullable(1));
+        Mono<Integer> justOrEmptyMono3 = Mono.justOrEmpty(1);
+        Mono<Object> justOrEmptyMono4 = Mono.justOrEmpty(null);
+
+
+        // Flux error
+        Flux<Object> errorFLux = Flux.error(new Exception("Something went wrong"));
+
+        // Flux duration
+        Flux.interval(Duration.ofMillis(1000));
 
         // Flux.fromIterable example
-        Flux<String> stringFlux = Flux.fromIterable(iterable);
+        Flux<String> stringFlux = Flux.fromIterable(list);
         List<String> strings = new ArrayList<>();
         stringFlux.subscribe(x -> strings.add(x));
 
@@ -40,6 +62,18 @@ public class FluxExamples {
         range.subscribe(System.out::println);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/flux/buffer")
+    public ResponseEntity<Flux> bufferFlux() {
+        // Empty flux
+        Flux<Integer> integerFlux = Flux.range(1,10);
+
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(integerFlux.buffer(2).buffer(Duration.ofMillis(5000)));
     }
 
     @GetMapping("/flux/withError")
@@ -129,9 +163,72 @@ public class FluxExamples {
                 .buffer(2);
     }
 
-    @GetMapping("/test")
-    public ResponseEntity<String> test() {
+    @GetMapping("/doOnNextExample")
+    public ResponseEntity<String> doOnNext() {
+        Flux.just(1,2,3,4)
+                .doOnEach(x -> System.out.println(x.getType()))
+                .subscribe(x -> System.out.println("subscriber value" + x));
+
         return ResponseEntity.ok("test");
     }
+
+    @GetMapping("/logExample")
+    public ResponseEntity<String> text() {
+        Flux.just(1,2,3,4)
+                .log()
+                .subscribe(x -> System.out.println("subscriber value" + x));
+
+        return ResponseEntity.ok("test");
+    }
+
+    @GetMapping("/delayExample")
+    public ResponseEntity<String> delayElement() {
+        Flux.just(1,2,3,4)
+                .delayElements(Duration.ofMillis(5000))
+                .subscribe(x -> System.out.println("subscriber value" + x));
+
+        return ResponseEntity.ok("test");
+    }
+
+
+    @GetMapping("/subscribeOnExample")
+    public ResponseEntity<String> subscribeOn() {
+        Flux.just(1,2,3,4)
+                .subscribeOn(Schedulers.parallel())
+                .subscribe(x -> System.out.println("subscriber value" + x));
+
+        return ResponseEntity.ok("test");
+    }
+
+    @GetMapping("/timeout")
+    public ResponseEntity<String> timeout() {
+        Flux.just(1,2,3,4)
+                .timeout(Duration.ofMillis(1000))
+                .subscribe(x -> System.out.println("subscriber value" + x));
+
+        return ResponseEntity.ok("test");
+    }
+
+    @GetMapping("/bufferUntilChanged")
+    public ResponseEntity<String> bufferUntilChanged() {
+        Flux.just(1,1,2,2,3,4)
+                .bufferUntilChanged()
+                .subscribe(x -> System.out.println("subscribed value" + x));
+
+        return ResponseEntity.ok("test");
+    }
+
+    @GetMapping("/cache")
+    public ResponseEntity<String> cache() {
+        Flux<Integer> flux = Flux.just(1, 1, 2, 2, 3, 4)
+                .cache(2);
+
+        flux.subscribe(System.out::println);
+
+        flux.subscribe(System.out::println);
+        return ResponseEntity.ok("test");
+    }
+
+
     // Cancelling a subscribe() with Its Disposable
 }
